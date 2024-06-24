406bfcf7dba719cb3189f55c5314ab41ddaa11d4
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-06-24 13:29:04 -0400

    starting o look like game now'

--- Scenes/bullet.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-var velocity = Vector2.RIGHT
-
-func _physics_process(delta):
-	position += velocity * delta
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,13 @@
 extends CharacterBody2D
 
-signal shoot
-var Bullet = preload("res://Scenes/bullet.tscn")
 @export var speed = 600
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var dir = get_global_mouse_position() - position
-		shoot.emit(Bullet, position, dir)
+	var input_direction = Input.get_axis("left", "right")
+	velocity.x = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	
-func _process(delta):
-	look_at(get_global_mouse_position())
+
