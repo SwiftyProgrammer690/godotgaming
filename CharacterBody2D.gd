13da05b31cae043a188906ae23c9e4744d1f9057
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-06-27 15:34:36 -0400

    good animations and physics

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,26 +2,32 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -1000.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d = $Sprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	if (velocity.x < 0 || velocity.x > 0):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
+	
+	var IS_LEFT = velocity.x < 0
+	sprite_2d.flip_h = IS_LEFT
