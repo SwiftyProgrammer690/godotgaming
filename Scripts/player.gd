f0360739cc4243263b49358beb85ef2e34e3919e
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-06-27 20:45:57 -0400

    its starting to look like a game now'

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,33 @@
 extends CharacterBody2D
 
-@export var speed = 600
 
-func get_input():
-	var input_direction = Input.get_axis("left", "right")
-	velocity.x = input_direction * speed
+const SPEED = 500.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d = $Sprite2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if (velocity.x < 0 || velocity.x > 0):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
 
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
+
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 30)
+
+	move_and_slide()
+	
+	var IS_LEFT = velocity.x < 0
+	sprite_2d.flip_h = IS_LEFT
