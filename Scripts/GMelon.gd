5ab455ca9124aded3ccdb687fa88926a704c721a
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-07-03 12:55:47 -0400

    finish level 5 and 6

---
+++ Scripts/GMelon.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+func _on_body_entered(body):
+	if (body.name == "Player"):
+		player.JUMP_VELOCITY = -6300
+		queue_free()
